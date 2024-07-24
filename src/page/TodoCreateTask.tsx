cb2c162fbfd5f35b5d8dfc599aeea68b237c7429
sistinelove <banan00110070@gmail.com>
2024-07-25 02:25:54 +0500

    Убрали состояние, и используем только форму

--- src/page/TodoCreateTask.tsx
+++ src/page/TodoCreateTask.tsx
@@ -1,25 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { useAppContext } from './ContextProvider.tsx';
 
 const TodoCreateTask = () => {
   const { addTodo } = useAppContext();
-  const [value, setValue] = useState('');
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    setValue(e.target.value);
-  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const value = formData.get('value') as string;
     await addTodo(value);
-    setValue('');
+    e.currentTarget.reset();
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" value={value} onChange={handleChange} />
+      <input type="text" name="value" />
       <input type="submit" />
     </form>
   );
